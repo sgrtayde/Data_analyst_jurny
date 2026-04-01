c48b0e9892543f8097087b9ce92bf7876678c745
Sagar tayde <sagartayde201@gmail.com>
2026-04-01 13:26:48 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,97 @@
+
+import os
+
+print("hello")
+
+print(os.listdir())
+
+print(os.getcwd())
+
+# Veriables
+
+sagar = 23
+
+its_admin = True
+
+its_me = False
+
+name = "sagar"
+
+city = "sambhaji_nagar"
+
+a = 4.8
+
+#  Veriables Type
+
+print(type(sagar))
+print(type(its_admin))
+print(type(its_me))
+print(type(name))
+print(type(city))
+print(type(a))
+
+
+
+# comments & multicomments
+
+# This will print hello word
+print("hello")   # somthing
+
+
+''' 
+this is 
+multiline comment
+
+'''
+
+# Type Convergen
+
+age = "56"
+
+print(int(age) + 34)
+
+print(int(age) + 10)
+
+a = "2356.83"
+print(float(a) + 12)
+
+d = 267
+print(str(d))
+
+
+# Taking input in python
+
+s = input("Enter your age :")
+print("my age is ",s)
+
+r = int(input("enter a number : "))
+print(r + 10)
+
+# Operators
+a = 10
+b = 3
+# arithmatic operators
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+
+
+# Comparison operator
+print(a == b)
+print(a != b)
+print(a < b)
+print(a <= b)
+print(a > b)
+print(a >= b)
+
+# Logical operator
+print(False and True)
+print(True and True)
+print(False or True)
+print(True or True)
+print(not(True))
+print(not(False))
